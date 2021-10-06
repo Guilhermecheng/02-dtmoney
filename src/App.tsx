14d603b160a2cd6e5f1e875f372e0376fd709d5d
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2021-10-06 00:24:14 -0300

    styling modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransaciontModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+
+	const [isNewTransacionModalOpen, setIsNewTransacionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransacionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransacionModalOpen(false);
+    }
+
 	return (
 		<>
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
+
+			<NewTransaciontModal isOpen={isNewTransacionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+
 			<GlobalStyle />
 		</>
 	);
